678f1bfe20b827ec937936fc616155a2bb0816d1
cauarojas <cauarojas@hotmail.com>
2022-05-19 21:24:33 -0300

    adicionei o boiler plate do codigo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { useState } from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
+import Content from './Content';
+import About from './About';
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>Hello, world!</h1>
+			<header></header>
+			<nav>
+				<Link to={'/'}>Inicio</Link>
+				<Link to={'/about'}>Saiba Mais</Link>
+			</nav>
+
+			<Routes>
+				<Route path='/' element={<Content />} />
+				<Route path='/about' element={<About />} />
+			</Routes>
 		</div>
 	);
 }
