a349d963345fd2a8814cff90bf3b9851c8f6088f
cauarojas <cauarojas@hotmail.com>
2022-05-19 21:36:33 -0300

    finished the basis for the project

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,25 @@ import { useState } from 'react';
 import { Link, Routes, Route } from 'react-router-dom';
 import Content from './Content';
 import About from './About';
+import logo from './semana.png';
+import Quiz from './Quiz';
 
 function App() {
 	return (
 		<div className='App'>
-			<header></header>
+			<header>
+				<img src={logo} alt='Semana Da Arte Moderna' />
+			</header>
 			<nav>
 				<Link to={'/'}>Inicio</Link>
 				<Link to={'/about'}>Saiba Mais</Link>
+				<Link to={'./quiz'}>Responda o Quiz</Link>
 			</nav>
 
 			<Routes>
 				<Route path='/' element={<Content />} />
 				<Route path='/about' element={<About />} />
+				<Route path='/quiz' element={<Quiz />} />
 			</Routes>
 		</div>
 	);
