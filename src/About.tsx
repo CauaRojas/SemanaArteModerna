94db82cb57846567c971eb74241c2a3fd28f6848
ReidakaiMFZ <richard_maldito2@hotmail.com>
2022-05-21 16:53:45 -0300

    adding an about page

--- src/About.tsx
+++ src/About.tsx
@@ -1,7 +1,24 @@
 export default function About() {
 	return (
 		<main>
-			<h1>About</h1>
+			<h1>Sobre nós</h1>
+			<p>	Somos alunos da escola estadual técnica (ETEC) Jaraguá, e estamos envolvidos 
+				na criação de um projeto relacionado a Semana da Arte Moderna.
+			</p>
+			<p>
+				Criamos este quiz para ajudar a nossos alunos a entender o que é, e o que foi 
+				a Semana da arte Moderna. Criando uma área de estudos e aprendizado para os alunos
+				assim como um ambiente de quiz para que eles possam se divertir e aprender.  
+			</p>
+			<h4>
+				Envolvidos no projeto:
+			</h4>
+			<ul>
+				<li>Cauã Rojas</li>
+				<li>Guilherme Luiz</li>
+				<li>Gustavo de Oliveira</li>
+				<li>Richard Sanches</li>
+			</ul>
 		</main>
 	);
 }
