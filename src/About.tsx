0bd0aae7e013633dcc4ce5bf1438fbe04d802a97
cauarojas <cauarojas@hotmail.com>
2022-05-26 22:22:04 -0300

    adicionei um section no about

--- src/About.tsx
+++ src/About.tsx
@@ -2,23 +2,27 @@ export default function About() {
 	return (
 		<main>
 			<h1>Sobre nós</h1>
-			<p>	Somos alunos da escola estadual técnica (ETEC) Jaraguá, e estamos envolvidos 
-				na criação de um projeto relacionado a Semana da Arte Moderna.
+			<p>
+				{' '}
+				Somos alunos da escola estadual técnica (ETEC) Jaraguá, e
+				estamos envolvidos na criação de um projeto relacionado a Semana
+				da Arte Moderna.
 			</p>
 			<p>
-				Criamos este quiz para ajudar a nossos alunos a entender o que é, e o que foi 
-				a Semana da arte Moderna. Criando uma área de estudos e aprendizado para os alunos
-				assim como um ambiente de quiz para que eles possam se divertir e aprender.
+				Criamos este quiz para ajudar a nossos alunos a entender o que
+				é, e o que foi a Semana da arte Moderna. Criando uma área de
+				estudos e aprendizado para os alunos assim como um ambiente de
+				quiz para que eles possam se divertir e aprender.
 			</p>
-			<h4>
-				Envolvidos no projeto:
-			</h4>
-			<ul>
-				<li>Cauã Rojas</li>
-				<li>Guilherme Luiz</li>
-				<li>Gustavo de Oliveira</li>
-				<li>Richard Sanches</li>
-			</ul>
+			<h4>Envolvidos no projeto:</h4>
+			<section>
+				<ul>
+					<li>Cauã Rojas</li>
+					<li>Guilherme Luiz</li>
+					<li>Gustavo de Oliveira</li>
+					<li>Richard Sanches</li>
+				</ul>
+			</section>
 		</main>
 	);
 }
